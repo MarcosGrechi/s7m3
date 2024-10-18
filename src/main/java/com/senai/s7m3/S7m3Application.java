d26c381d2a42f3656562ced4af0b8030937cba48
MarcosGrechi <marcosgrechi@gmail.com>
2024-10-18 20:45:12 -0300

    first commit

---
+++ src/main/java/com/senai/s7m3/S7m3Application.java
@@ -0,0 +1,13 @@
+package com.senai.s7m3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7m3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7m3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/s7m3/S7m3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.s7m3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7m3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
